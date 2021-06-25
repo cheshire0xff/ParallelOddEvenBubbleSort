30c63e7fc204ee7b7dd8d518e863fbae02a3fe3d
cheshire <cheshire0xff@gmail.com>
2021-06-25 23:18:48 +0200

    tests sh uses test indexing

--- tests.sh
+++ tests.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-for i in $(seq 2 60); do 
-	mpirun -n $i ./main 100 --log --delay 10 || exit 1; 
-done
-for i in $(seq 2 60); do 
-	mpirun -n $i ./main 100 --log --delay 1 || exit 2; 
-done
+if [ $1 -eq 1 ] ; then
+	for i in $(seq 2 60); do 
+		mpirun -n $i ./main 100 --log --delay 10 || exit 1; 
+	done
+elif [ $1 -eq 2 ] ; then
+	for i in $(seq 2 60); do 
+		mpirun -n $i ./main 100 --log --delay 1 || exit 2; 
+	done
+elif [ $1 -eq 3 ] ; then
+	for i in $(seq 10 200); do 
+		mpirun -n 9 ./main $i --log --delay 1 || exit 2; 
+	done
+else 
+	echo "No test with this id!"
+	exit 1;
+fi
